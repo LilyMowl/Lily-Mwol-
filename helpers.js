a5bc55f7d2ec21830ad04d215f40e6ef42a87b9b
NKMODS <86371273+NKMODS@users.noreply.github.com>
2021-09-09 13:44:17 +0530

    helpers.js
    Only Lily Mwol

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Julie Mwol*:  ```" + msg + "```"
+    return "✅ *Lily Mwol*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Julie Mwol*:  ```" + msg + "```"
+    return "🛑 *Lily Mwol*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Julie Mwol*:  ```" + msg + "```"
+    return "⏺️ *Lily Mwol*:  ```" + msg + "```"
 }
 
 
